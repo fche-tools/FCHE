f8900bc5431611221ed48182fe8d1344f2e85e48
weishaozhi <814240381@qq.com>
2020-10-25 11:28:36 +0800

    Changed the way to filter test files.

--- src/main/java/entitybuilder/BuilderIntf.java
+++ src/main/java/entitybuilder/BuilderIntf.java
@@ -24,7 +24,7 @@ public class BuilderIntf {
     private void setVisitor(String fileFullPath) {
 
         if (configure.getLang().equals(Configure.JAVA_LANG)) {
-            if(!fileFullPath.endsWith("_test.java")) {
+            if(!fileFullPath.contains("test") && !fileFullPath.contains("Test")) {
                 visitor = new JavaEntityVisitor(fileFullPath);
             }
         }
